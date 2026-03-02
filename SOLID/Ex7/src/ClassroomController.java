0acd8f3e744679dcf0f2f876d7791db8dea966f1
Parth Dagia <parth.24bcs10414@sst.scaler.com>
2026-03-02 14:28:54 +0530

    Refactor Ex7 using Interface Segregation Principle (ISP): split fat interface into capability-based interfaces

--- SOLID/Ex7/src/ClassroomController.java
+++ SOLID/Ex7/src/ClassroomController.java
@@ -1,27 +1,38 @@
 public class ClassroomController {
+
     private final DeviceRegistry reg;
 
-    public ClassroomController(DeviceRegistry reg) { this.reg = reg; }
+    public ClassroomController(DeviceRegistry reg) {
+        this.reg = reg;
+    }
 
     public void startClass() {
-        SmartClassroomDevice pj = reg.getFirstOfType("Projector");
-        pj.powerOn();
-        pj.connectInput("HDMI-1");
 
-        SmartClassroomDevice lights = reg.getFirstOfType("LightsPanel");
+        PowerControllable pjPower = reg.getFirstOfType(Projector.class);
+        InputConnectable pjInput = reg.getFirstOfType(Projector.class);
+
+        pjPower.powerOn();
+        pjInput.connectInput("HDMI-1");
+
+        BrightnessControllable lights =
+                reg.getFirstOfType(LightsPanel.class);
         lights.setBrightness(60);
 
-        SmartClassroomDevice ac = reg.getFirstOfType("AirConditioner");
+        TemperatureControllable ac =
+                reg.getFirstOfType(AirConditioner.class);
         ac.setTemperatureC(24);
 
-        SmartClassroomDevice scan = reg.getFirstOfType("AttendanceScanner");
-        System.out.println("Attendance scanned: present=" + scan.scanAttendance());
+        AttendanceScannable scan =
+                reg.getFirstOfType(AttendanceScanner.class);
+        System.out.println("Attendance scanned: present=" +
+                scan.scanAttendance());
     }
 
     public void endClass() {
         System.out.println("Shutdown sequence:");
-        reg.getFirstOfType("Projector").powerOff();
-        reg.getFirstOfType("LightsPanel").powerOff();
-        reg.getFirstOfType("AirConditioner").powerOff();
+
+        reg.getFirstOfType(Projector.class).powerOff();
+        reg.getFirstOfType(LightsPanel.class).powerOff();
+        reg.getFirstOfType(AirConditioner.class).powerOff();
     }
 }
